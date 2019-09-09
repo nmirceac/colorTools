53fb0fc559e6f74831a0415544dc694871bb0344
DevBox <dev@weanswer.it>
2019-09-09 15:54:35 +0200

    color tools controller

---
+++ src/Http/Controllers/ImagesController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace ColorTools\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImagesController extends \App\Http\Controllers\Controller
+{
+    public function index($urlString='')
+    {
+        $type='jpeg';
+        if(strrpos($urlString, '.')) {
+            $type = substr($urlString, 1 + strrpos($urlString, '.'));
+        }
+
+        $store = \ColorTools\Store::findAndProcess($urlString);
+        $store->publish($type);
+        header('Content-type: image/'.$type);
+        echo file_get_contents($store->getPublishPath($type));
+    }
+
+    public function histogram($type='c', $id)
+    {
+        return \App\ImageStore::find($id)->showHistogram($type);
+    }
+
+    public function upload()
+    {
+        $roleName = request('role');
+
+        $excludeFromDetails = ['file', 'modelId', 'model', 'single', 'role', $roleName];
+        $details = request()->except($excludeFromDetails);
+
+        $image = \App\ImageStore::createFromRequest(request(), $roleName);
+
+
+        if (request('model', false) and request('modelId', false)) {
+
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+
+            if (request('single', false)) {
+                $image = $image->set($object, $roleName, $details);
+            } else {
+                $image = $image->attach($object, $roleName, 'last', $details);
+            }
+
+        }
+
+        return response()->json([$this->itemName => $image]);
+    }
+
+    public function attach()
+    {
+        $image = \App\ImageStore::findOrFail(request('id'));
+
+        $roleName = request('role');
+
+        if (request('model', false) and request('modelId', false)) {
+
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+
+            if (request('single', false)) {
+                $image = $image->set($object, $roleName, []);
+            } else {
+                $image = $image->attach($object, $roleName, 'last', []);
+            }
+
+        }
+
+        return response()->json([$this->itemName => $image]);
+    }
+
+    public function delete()
+    {
+        $image = \App\ImageStore::findOrFail(request('id'));
+
+        if (request('model', false) and request('modelId', false)) {
+
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+
+            if (request('id')) {
+                $object->clearImage(request('id'));
+            } elseif (request('role')) {
+                $object->clearImagesByRole(request('role'));
+            }
+        }
+
+        $image->delete();
+    }
+
+    public function replace()
+    {
+        $originalImage = \App\ImageStore::findOrFail(request('id'));
+        $roleName = request('role');
+
+        $newImage = $originalImage->replaceFromRequest(request(), $roleName);
+
+        $imageMeta = (array)$newImage->metadata;
+
+        $metadata = request()->only(['caption', 'copyright', 'tags']);
+        foreach ($metadata as $key => $data) {
+            if ($data == "null" || is_null($data)) {
+                if ($key !== 'tags') {
+                    $imageMeta[$key] = '';
+                } else {
+                    $imageMeta[$key] = [];
+                }
+            } elseif ($key == 'tags') {
+                $imageMeta['tags'] = explode(',', $data);
+            } else {
+                $imageMeta[$key] = $data;
+            }
+        }
+
+        $newImage->metadata = $imageMeta;
+        $newImage->update();
+
+        $newImage->log(Log::LOG_IMAGE_REPLACED);
+
+        return response()->json([$this->itemName => $newImage]);
+    }
+
+    public function preview($imageId)
+    {
+        $image = \App\ImageStore::findOrFail($imageId);
+
+        return $image->serve();
+    }
+
+    public function download($imageId)
+    {
+        $image = \App\ImageStore::findOrFail($imageId);
+
+        return $image->serveForceDownload();
+    }
+
+    public function update()
+    {
+        $image = \App\ImageStore::findOrFail(request('id'));
+
+        if (request('model', false) and request('modelId', false)) {
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+            $image->attach($object, request('role'), request('order'), request('metadata'));
+        } else {
+            $image->metadata = request('metadata');
+            $image->update();
+        }
+
+
+        return response()->json([$this->itemName => $image]);
+    }
+
+    public function reorder()
+    {
+        $model = '\\App\\' . ucfirst(request('model'));
+        $model = new $model;
+        $object = $model->find(request('modelId'));
+        $object->reorderImagesByRole(request('ids'), request('role'));
+    }
+
+}
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,9 +1,15 @@
 <?php
 
-$router->get('/{image}', ['uses'=>'ImageController@index', 'as'=>config('colortools.router.namedPrefix').'.get']);
-
+$router->get('/{image}', ['uses'=>'ImagesController@index', 'as'=>config('colortools.router.namedPrefix').'.get']);
 
 $router->group(['middleware' => config('colortools.router.authMiddleware')], function ($router) {
-    $router->get('/h/{type}/{image}', ['uses'=>'ImageController@histogram', 'as'=>config('colortools.router.namedPrefix').'.histogram']);
-//    $router->get('/{image}', ['uses'=>'ImageController@index', 'as'=>config('colortools.router.namedPrefix').'.get']);
+    $router->get('/h/{type}/{image}', ['uses'=>'ImagesController@histogram', 'as'=>config('colortools.router.namedPrefix').'.histogram']);
+    $router->get('/preview/{image}', ['uses'=>'ImagesController@preview', 'as'=>config('colortools.router.namedPrefix').'.preview']);
+    $router->get('/download/{image}', ['uses'=>'ImagesController@image', 'as'=>config('colortools.router.namedPrefix').'.image']);
+    $router->post('/upload', ['uses'=>'ImagesController@upload', 'as'=>config('colortools.router.namedPrefix').'.upload']);
+    $router->post('/attach', ['uses'=>'ImagesController@attach', 'as'=>config('colortools.router.namedPrefix').'.attach']);
+    $router->post('/delete', ['uses'=>'ImagesController@delete', 'as'=>config('colortools.router.namedPrefix').'.delete']);
+    $router->post('/replace', ['uses'=>'ImagesController@replace', 'as'=>config('colortools.router.namedPrefix').'.replace']);
+    $router->post('/update', ['uses'=>'ImagesController@update', 'as'=>config('colortools.router.namedPrefix').'.update']);
+    $router->post('/reorder', ['uses'=>'ImagesController@reorder', 'as'=>config('colortools.router.namedPrefix').'.reorder']);
 });
